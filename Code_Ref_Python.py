8929eed23925a2009d86243b55b9085eccfe5540
Teo-FREJA <teo.frejaville@etu.u-bordeaux.fr>
2021-10-05 13:23:22 +0200

    Test

---
+++ Code_Ref_Python.py
@@ -0,0 +1,29 @@
+# Condition/Tests/ Boucle
+    def cond(a,b,c,d):
+        if(a != b):
+            return c
+        else:
+            return d
+
+    def boucle_for(n):
+        for i in range(n):
+            i=i+1
+        return i 
+
+    def boucle_while(n):
+        i = 1
+        while(i<n):
+            i = i + 1 
+        return i
+
+# Dictionnaire
+    dict = { "toto" : 1, "tata" : 2, "titi" : 3 }
+    # Attribution valeur à des chaines de caractères/Variable 
+
+# Library
+import numpy
+import math
+import sys
+import time
+import pygame
+import serial
