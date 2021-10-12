02ba74e100e42b58c1f20dd484cb695e21966515
RonanLc <63312804+RonanLc@users.noreply.github.com>
2021-10-12 10:36:06 +0200

    Arduino code for the first SPI test

---
+++ Arduino/Test_communication_SPI.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+
+const int SlaveSelect = 10; //pin du Slave Select
+// MOSI -> pin 11
+// MISO -> pin 12
+// SCK  -> pin 13
+
+//Arduino is the Master
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(SlaveSelect, OUTPUT);
+  SPI.begin();
+
+}
+
+void loop() {
+
+  delay(1000);
+  
+  digitalWrite(SlaveSelect, 1);
+  
+  SPI.transfer("01000001"); //Envoie lettre A
+  Serial.println("A : 01000001");
+
+  digitalWrite(SlaveSelect, 0);
+  
+  while(1);
+
+}
