13df0f0311c01ff47effaee8e1732a493a8753c5
Ronan Lc <lecorroncronan@gmail.com>
2022-03-06 17:19:13 +0100

    Modification of the SPI communication to send and receive data from an 8 neural network

--- Arduino/Master.ino
+++ Arduino/Master.ino
@@ -5,6 +5,7 @@
 #define bp 3
 
 const int slaveSelect = 10;
+unsigned char tps_com[5];
 
 void setup() {
 
@@ -12,7 +13,13 @@ void setup() {
   pinMode(slaveSelect, OUTPUT);
   pinMode(bp, INPUT_PULLUP);
   Serial.begin(9600);
-  
+
+  tps_com[0] = 0b10010011;
+  tps_com[1] = 0b01110101;
+  tps_com[2] = 0b10011010;
+  tps_com[3] = 0b01010100;
+  tps_com[4] = 0b01100011;
+
 }
 
 void loop() {
@@ -20,9 +27,12 @@ void loop() {
   if (digitalRead(bp) == 0){
     
     digitalWrite(slaveSelect, 0);
-    SPI.transfer('B');
-    delay(100);
-    digitalWrite(slaveSelect, 1);
     
+    for (int i = 0; i < 5; i++){
+      SPI.transfer(tps_com[i]);
+      delay(10);
+    }
+
+    digitalWrite(slaveSelect, 1);
   }
 }
--- py/raspi.py
+++ py/raspi.py
@@ -1,6 +1,7 @@
 
 ##### Code fonctionnel pour reception de décimaux
 
+from re import S
 import spidev
 import RPi.GPIO as GPIO
 import time
@@ -8,6 +9,9 @@ import time
 LED = 21
 GPIO.setmode(GPIO.BCM) #on utilise les numéro des broches (pas des GPIO)
 GPIO.setup(LED,GPIO.OUT) #broche 21 en sortie
+
+tram = [] #tableau stockant toutes les trams reçu en fin de communication
+tps_com = 10 #durée de la communication (nombre de trams que l'on va recevoir)
 
 spi_bus = 0
 
@@ -20,34 +24,36 @@ spi.open(spi_bus, spi_device)
 spi.max_speed_hz = 1000000
 # Send a null byte to check for value
 
-send_byte = 0x80
+for i in range(tps_com):
+	send_byte = 0x80
 
-rcv_byte = spi.xfer2([send_byte])
+	rcv_byte = spi.xfer2([send_byte])
 
-# repeat to check for a response
+	# repeat to check for a response
 
-rcv_byte = spi.xfer2([send_byte])
+	rcv_byte = spi.xfer2([send_byte])
 
-data_recv = rcv_byte[0]
-if (data_recv != 0x80):
-	print("ready to communicate"+str(data_recv)) 
-	print(data_recv) #affiche la donné envoyé par l'arduino
-	if (data_recv == 0x0):
-		GPIO.output(LED,GPIO.HIGH) # allume la LED
-	if (data_recv == 0x1):
-		GPIO.output(LED,GPIO.LOW) # éteint la LED
-	quit()
+	data_recv = rcv_byte[0]
+	if (data_recv != 0x80):
+		print("ready to communicate"+str(data_recv)) 
+		print(data_recv) #affiche la donné envoyé par l'arduino
+		tram.append(bin(data_recv))
+		quit()
 
-# transmission d'un nombre binaire en python
-# on prendra un nombre en hexa quelconque 
+for i in range(len(tram)):
+	print(tram[i])
 
-a = bin(0x1) # on envoie 0x1 en binaire sous la forme '0b1' -> le 0b car binaire
-a = a[2::] # supprime le 0b pour obtenir que le nombre binaire
-print(a) # a = 1
 
-b = bin(0x0) # ici on envoie 0 en binaire 
-b = b[2::]
-print(b)
+	# transmission d'un nombre binaire en python
+	# on prendra un nombre en hexa quelconque 
 
-# on a ainsi des bit 1 et 0 que l'on peut envoyer à la suite
-# il faut juste traduire en language C pour arduino
+#	a = bin(0x1) # on envoie 0x1 en binaire sous la forme '0b1' -> le 0b car binaire
+#	a = a[2::] # supprime le 0b pour obtenir que le nombre binaire
+#	print(a) # a = 1
+
+#	b = bin(0x0) # ici on envoie 0 en binaire 
+#	b = b[2::]
+#	print(b)
+
+	# on a ainsi des bit 1 et 0 que l'on peut envoyer à la suite
+	# il faut juste traduire en language C pour arduino
